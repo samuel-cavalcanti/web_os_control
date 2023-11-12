7d12657556a90826d93830336c7f8a3671d4ee81
samuel-cavalcanti <scavalcanti111@gmail.com>
2023-11-12 20:02:55 -0300

    disable `wait_until_tv_is_on`

--- web_os/src/discovery_tv.rs
+++ web_os/src/discovery_tv.rs
@@ -36,51 +36,51 @@ pub async fn search_tv_task<S: SearchTvs>(
     }
 }
 
-pub async fn wait_until_tv_is_on<S: SearchTvs>(
-    tv: &WebOsNetworkInfo,
-    delay_in_ms: u64,
-    search_method: &mut S,
-) -> Result<(), String> {
-    let find_tv = |infos: &Vec<WebOsNetworkInfo>| {
-        infos
-            .iter()
-            .filter(|i| i.mac_address == tv.mac_address)
-            .count()
-            > 0
-    };
-
-    let mut infos = search_method.search().await?;
-
-    let max_try = 15;
-
-    for _ in 0..max_try {
-        if find_tv(&infos) {
-            log::debug!("tvs: {infos:?}");
-            return Ok(());
-        }
-
-        tokio::time::sleep(tokio::time::Duration::from_millis(delay_in_ms)).await;
-
-        infos = search_method.search().await?;
-    }
-
-    Err("Execed Max Tries: {max_try}".into())
-}
+// pub async fn wait_until_tv_is_on<S: SearchTvs>(
+//     tv: &WebOsNetworkInfo,
+//     delay_in_ms: u64,
+//     search_method: &mut S,
+// ) -> Result<(), String> {
+//     let find_tv = |infos: &Vec<WebOsNetworkInfo>| {
+//         infos
+//             .iter()
+//             .filter(|i| i.mac_address == tv.mac_address)
+//             .count()
+//             > 0
+//     };
+//
+//     let mut infos = search_method.search().await?;
+//
+//     let max_try = 15;
+//
+//     for _ in 0..max_try {
+//         if find_tv(&infos) {
+//             log::debug!("tvs: {infos:?}");
+//             return Ok(());
+//         }
+//
+//         tokio::time::sleep(tokio::time::Duration::from_millis(delay_in_ms)).await;
+//
+//         infos = search_method.search().await?;
+//     }
+//
+//     Err("Execed Max Tries: {max_try}".into())
+// }
 
 #[cfg(test)]
 mod tests {
 
     use lg_webos_client::discovery::WebOsNetworkInfo;
 
-    use crate::mocks::{MockSucessSearch, MockErrorSearch};
+    use crate::mocks::{MockSucessSearch, /* MockErrorSearch */};
 
-    use super::{search_tv_task, wait_until_tv_is_on};
+    use super::{search_tv_task, /* wait_until_tv_is_on */};
 
 
 
     #[tokio::test]
     async fn test_search_tv_task() {
-        let expected_tv = vec![
+        let expected_tv = [
             "192.168.0.199",
             "WebOS/1.5 UPnP/1.0 webOSTV/1.0",
             "03:a1:11:a6:0f:3e",
@@ -103,33 +103,33 @@ mod tests {
             assert_eq!(tv[i], expected_tv[i].to_string())
         }
     }
-    #[tokio::test]
-    async fn test_wait_until_tv_is_on() {
-        let mut mock = MockSucessSearch {
-            infos: vec![WebOsNetworkInfo {
-                ip: "192.168.0.199".into(),
-                name: "WebOS/1.5 UPnP/1.0 webOSTV/1.0".into(),
-                mac_address: "03:a1:11:a6:0f:3e".into(),
-            }],
-        };
-
-        let info = WebOsNetworkInfo {
-            ip: "192.168.0.199".into(),
-            name: "WebOS/1.5 UPnP/1.0 webOSTV/1.0".into(),
-            mac_address: "03:a1:11:a6:0f:3e".into(),
-        };
-
-        wait_until_tv_is_on(&info, 0, &mut mock).await.unwrap();
-
-        mock = MockSucessSearch{infos:vec![]};
-
-        let error =wait_until_tv_is_on(&info, 0, &mut mock).await;
-        assert!(error.is_err());
-
-        let mut mock = MockErrorSearch;
-
-        let error =wait_until_tv_is_on(&info, 0, &mut mock).await;
-        assert!(error.is_err());
-
-    }
+    // #[tokio::test]
+    // async fn test_wait_until_tv_is_on() {
+    //     let mut mock = MockSucessSearch {
+    //         infos: vec![WebOsNetworkInfo {
+    //             ip: "192.168.0.199".into(),
+    //             name: "WebOS/1.5 UPnP/1.0 webOSTV/1.0".into(),
+    //             mac_address: "03:a1:11:a6:0f:3e".into(),
+    //         }],
+    //     };
+    //
+    //     let info = WebOsNetworkInfo {
+    //         ip: "192.168.0.199".into(),
+    //         name: "WebOS/1.5 UPnP/1.0 webOSTV/1.0".into(),
+    //         mac_address: "03:a1:11:a6:0f:3e".into(),
+    //     };
+    //
+    //     wait_until_tv_is_on(&info, 0, &mut mock).await.unwrap();
+    //
+    //     mock = MockSucessSearch{infos:vec![]};
+    //
+    //     let error =wait_until_tv_is_on(&info, 0, &mut mock).await;
+    //     assert!(error.is_err());
+    //
+    //     let mut mock = MockErrorSearch;
+    //
+    //     let error =wait_until_tv_is_on(&info, 0, &mut mock).await;
+    //     assert!(error.is_err());
+    //
+    // }
 }
